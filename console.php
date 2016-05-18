ba7542d3b09e79b6aad9c598ba1320aa947da5fb
juanber84 <juanber84@gmail.com>
2016-05-18 20:41:45 +0200

    Timezone travis

--- console.php
+++ console.php
@@ -2,6 +2,8 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
+date_default_timezone_set('UTC');
+
 $console = new \Juanber84\Console\Application();
 $console->addCommands(array(
     new \Juanber84\Console\Command\ShowProjectsCommand(),
