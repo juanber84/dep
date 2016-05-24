8826deb48829a7eec9e19f2536166e9944ddbe70
juanber84 <juanber84@gmail.com>
2016-05-24 16:20:19 +0200

    Download in local bin path

--- src/Juanber84/Services/DownloadService.php
+++ src/Juanber84/Services/DownloadService.php
@@ -6,13 +6,15 @@ class DownloadService
 {
     public function download($url)
     {
+        // Todo: refactor and include in configuration
+        $pathExec = "/usr/local/bin";
         try {
             $content = file_get_contents($url);
-            file_put_contents("./newdep.phar", $content);
-            unlink('./dep.phar');
-            $content = file_get_contents("./newdep.phar");
-            file_put_contents("dep.phar", $content);
-            unlink('./newdep.phar');
+            file_put_contents($pathExec."/newdep.phar", $content);
+            unlink($pathExec.'/dep.phar');
+            $content = file_get_contents($pathExec."/newdep.phar");
+            file_put_contents($pathExec."/dep.phar", $content);
+            unlink($pathExec.'/newdep.phar');
         } catch (\Exception $e){
             return false;
         }
